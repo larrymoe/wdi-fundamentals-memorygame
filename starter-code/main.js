5b9ef2add84ada42d211c4f8e1cf462e17bb81f7
larrymoe <invaderska@gmail.com>
2017-01-23 16:58:15 -0500

    added JS function to create cards, removed HTML

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,26 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log('JS file is connected to HTML! Woo!');
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
 
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
 	alert('You found a match!');
 } else {
 	altert('Sorry, try again.');
+}*/
+
+var board = document.getElementById('game-board');
+
+function createCards() {
+
+	for (var i = 0; i < 4; i++) {
+		var card = document.createElement('div');
+		card.className = 'card';
+		board.appendChild(card);
+	}
+
 }
+
+createCards();
